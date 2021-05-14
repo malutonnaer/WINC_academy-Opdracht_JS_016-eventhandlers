bf16e420c8dcb514ea1a170c46248ec0ec050b39
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-14 13:17:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// var buttonAlert = document.getElementById('mybutton');
+
+// buttonAlert.addEventListener("click", makeAlert);
+
+// function makeAlert() {
+//     alert("Button Clicked")
+// };
+
+const buttonAlert = document.getElementById('mybutton');
+buttonAlert.addEventListener("click", function () {
+    alert("Button Clicked!");
+});
+
+
+
